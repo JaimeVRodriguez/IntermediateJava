aaed9dd050bc72a987a558068d405ad3c095b955
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-08-18 19:55:54 -0400

    Add Error fix assignment

--- Week1/Debug1/src/DebugOne.java
+++ Week1/Debug1/src/DebugOne.java
@@ -1,3 +1,19 @@
+/*
+ * Jaime Rodriguez
+ * Assignment 1.2
+ * August 18, 2020
+ *
+ * Purpose: Purposely insert "bugs" for
+ * other students to find and fix
+ *
+ * To compile the program:
+ * javac DebugOne.java
+ *
+ * To execute the program:
+ * java DebugOne
+ *
+ */
+
 import javax.swing.*;
 
 public class DebugOne {
@@ -8,12 +24,15 @@ public class DebugOne {
         int numberOne, numberTwo, total;
 
         JOptionPane.showMessageDialog(null, "Your two number will be added together.");
+        // Missing "J"
         userInputOne = OptionPane.showInputDialog(null, "Enter your first a whole number");
         numberOne = Integer.parseInt(userInputOne);
 
         userInputTwo = JOptionPane.showInputDialog(null, "Enter your second number");
+        // Missing ";"
         numberTwo = Integer.parseInt(userInputTwo)
 
+        // userInputOne and userInput Two should be numberOne and numberTwo
         total = userInputOne + userInputTwo;
 
         JOptionPane.showMessageDialog(null, numberOne + " + " + numberTwo + " = " + total);
