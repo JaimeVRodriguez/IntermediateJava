6162cd98dbdc8141135633e1369f1da294c870eb
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-08-24 13:10:33 -0400

    Reformat utilizing loop

--- Week2/CollegeStudent/src/CollegeStudent.java
+++ Week2/CollegeStudent/src/CollegeStudent.java
@@ -2,15 +2,11 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.Scanner;
 
 public class CollegeStudent {
 
-    private String firstName;
-    private String lastName;
-    private GregorianCalendar enrollmentDate;
-    private GregorianCalendar projectedGradDate;
-    public Scanner scanner = new Scanner(System.in);
+    private String firstName, lastName;
+    private GregorianCalendar enrollmentDate, projectedGradDate;
 
     public CollegeStudent(String firstName, String lastName, GregorianCalendar enrollmentDate) {
         this.firstName = firstName;
@@ -21,7 +17,7 @@ public class CollegeStudent {
         int month = enrollmentDate.get(Calendar.MONTH);
         int day = enrollmentDate.get(Calendar.DAY_OF_MONTH);
 
-        GregorianCalendar gc = new GregorianCalendar(year +4, month, day);
+        GregorianCalendar gc = new GregorianCalendar(year+4, month, day);
 
         setProjectedGradDate(gc);
     }
@@ -62,7 +58,4 @@ public class CollegeStudent {
     public String toString() {
         return "First Name : "+firstName+"\t Last Name : "+lastName+"\t EnrollDate : "+format(enrollmentDate)+"\t Graduation Date : "+format(projectedGradDate);
     }
-
-
-
 }
